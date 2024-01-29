7f3f9197d30be48affd4844694e92d94f9582a0e
Mahfuz Hasan Reza <mahfuzhasanreza1@gmail.com>
2024-01-29 16:41:50 +0600

    modify the code

--- coordinatesDisCal.cpp
+++ coordinatesDisCal.cpp
@@ -4,34 +4,34 @@ using namespace std;
 double totalDis=0;
 double minDis=0;
 double tempMinDis=0;
-double minLon1=0, minLat1=0, minLon2=0, minLat2=0;
+double minLat1=0, minLon1=0, minLat2=0, minLon2=0;
 
-void minDisCal(double lon1, double lat1, double lon2, double lat2, double distance){
+void minDisCal(double lat1, double lon1, double lat2, double lon2, double distance){
     if(!tempMinDis){
         tempMinDis=1;
         minDis=distance;
-        minLon1=lon1;
         minLat1=lat1;
-        minLon2=lon2;
+        minLon1=lon1;
         minLat2=lat2;
+        minLon2=lon2;
         return;
     }
 
     if(minDis>distance){
         minDis=distance;
-        minLon1=lon1;
         minLat1=lat1;
-        minLon2=lon2;
+        minLon1=lon1;
         minLat2=lat2;
+        minLon2=lon2;
     }
 }
 
-double disCal(double lon1, double lat1, double lon2, double lat2) {
+double disCal(double lat1, double lon1, double lat2, double lon2) {
     double distance=acos(sin(lat1*(M_PI/180.0))*sin(lat2*(M_PI/180.0))+cos(lat1*(M_PI/180.0))*cos(lat2*(M_PI/180.0))*cos(lon2*(M_PI/180.0)-lon1*(M_PI/180.0)))*6371;
     distance*=1000;
     totalDis+=distance;
 
-    minDisCal(lon1, lat1, lon2, lat2, distance);
+    minDisCal(lat1, lon1, lat2, lon2, distance);
 
     return distance;
 }
@@ -40,23 +40,23 @@ int main(){
     int n;
     cin>>n;
 
-    double lon2, lat2;
-    if(n>0) cin>>lon2>>lat2;
+    double lat2, lon2;
+    if(n>0) cin>>lat2>>lon2;
     for(int i=0; i<n-1; i++){
-        double lon1=lon2, lat1=lat2;
-        cin>>lon2>>lat2;
-        cout<<fixed<<setprecision(8)<<"Point "<<i+1<<": ("<<lon1<<", "<<lat1<<")"<<endl;
-        cout<<fixed<<setprecision(8)<<"Point "<<i+2<<": ("<<lon2<<", "<<lat2<<")"<<endl;
+        double lat1=lat2, lon1=lon2;
+        cin>>lat2>>lon2;
+        cout<<fixed<<setprecision(8)<<"Point "<<i+1<<": ("<<lat1<<", "<<lon1<<")"<<endl;
+        cout<<fixed<<setprecision(8)<<"Point "<<i+2<<": ("<<lat2<<", "<<lon2<<")"<<endl;
     
-        double distance = disCal(lon1, lat1, lon2, lat2);
+        double distance = disCal(lat1, lon1, lat2, lon2);
         
         cout<<"Distance between point "<<i+1<<" and "<<i+2<<": "<<distance<<" meter"<<endl;
     }
 
     cout<<endl<<"Total distance among all points: "<<totalDis<<" meter"<<endl;
     cout<<"Minimum ditsance among all points: "<<minDis<<" meter"<<endl;
-    cout<<endl<<"Minimum distance point 1 (longitude, latitude): ("<<minLon1<<", "<<minLat1<<")"<<endl;
-    cout<<"Minimum distance point 2 (longitude, latitude): ("<<minLon2<<", "<<minLat2<<")"<<endl;
+    cout<<endl<<"Minimum distance point 1 (latitude, longitude): ("<<minLat1<<", "<<minLon1<<")"<<endl;
+    cout<<"Minimum distance point 2 (latitude, longitude): ("<<minLat2<<", "<<minLon2<<")"<<endl;
 
     return 0;
 }
